5e917cc6a7ab9f29b428e8834b6a08060a1d05c7
Michael Bishopp <mdbishopp@hotmail.com>
2017-01-11 14:44:16 -0800

    Added PUT to todos

--- server.js
+++ server.js
@@ -54,8 +54,6 @@ app.post('/todos', function (req, res) {
     body.description = body.description.trim();
 
     // add id field
-    // increment todoNextId
-    // push body into array
     body.id = todoNextId;
     todoNextId++;
 
@@ -85,6 +83,37 @@ app.delete('/todos/:id', function (req, res) {
 
     //send 200
     //send deleted item
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    } else {
+        // Never provided attribute, no problem here
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+    
 });
 
 app.listen(PORT, function () {
