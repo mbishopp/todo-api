e9552cf38847c95f13a5e0749f564ad1f0ecb942
Michael Bishopp <mdbishopp@hotmail.com>
2017-01-11 12:55:36 -0800

    added DELETE to todo API

--- server.js
+++ server.js
@@ -67,6 +67,26 @@ app.post('/todos', function (req, res) {
     res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    //_.without
+    
+    //res.json(newTodos);
+    //todos === newTodos;
+  
+    if (matchedTodo) {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    } else {
+    res.status(404).json({"error": "no todo found with that id"})
+    }
+
+    //send 200
+    //send deleted item
+});
+
 app.listen(PORT, function () {
     console.log('Express listening on Port: ' + PORT);
 });
