45da287f00ac6cef2159aca85e4a0d613951e4f7
Michael Bishopp <mdbishopp@hotmail.com>
2017-01-11 10:22:26 -0800

    Added post

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
-var todos = [{
-    id: 1,
-    description: 'Meet Dad for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Walk the dog',
-    completed: true
-}];
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
@@ -44,6 +36,22 @@ app.get('/todos/:id', function (req, res) {
         res.status(404).send();
     }
     
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+
+    // add id field
+    // increment todoNextId
+    // push body into array
+    body.id = todoNextId;
+    todos.push(body);
+    todoNextId++;
+
+    //console.log('description: ' + body.description);
+
+    res.json(body);
 });
 
 app.listen(PORT, function () {
