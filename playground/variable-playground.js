e1af7b37cb191795305c59b3e42057c9ae6c3d65
Michael Bishopp <mdbishopp@hotmail.com>
2017-01-30 16:28:46 -0800

    Add sequelize code

---
+++ playground/variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+//     name: 'Michael',
+//     age: 21
+// };
+
+// function updatePerson (obj) {
+//     // obj = {
+//     //     name: 'Michael',
+//     //     age: 24
+//     // };
+//     obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+
+var grades = ['A', 'B'];
+
+function updateGrades (arr) {
+    arr.push('C');
+    debugger;
+}
+
+updateGrades(grades);
+console.log(grades);
+
